f0c0b76f7d57132386eb6b7f831e8b79ebbf77bd
mertma52 <79012765+mertma52@users.noreply.github.com>
2022-12-04 17:52:33 +0100

    Notiz

---
+++ notes.js
@@ -0,0 +1,4 @@
+//and sudo is just general linux knowledge, a way to run code with elevated privileges
+// --force in general is usually a way to tell the command you're running that you know what you're doing and want it to ignore certain things that might be dangerous. In this case, it says this in the error:
+// npm ERR! Remove the existing file and try again, or run npm
+// npm ERR! with --force to overwrite files recklessly.
